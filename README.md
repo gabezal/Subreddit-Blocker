ead108c559c33c82c567f169b58f872af76bd723
Owen Haggerty <teridax2@gmail.com>
2022-02-14 14:34:23 -0800

    Added README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Subreddit Blocker
+This is a firefox extension which hides posts from user chosen subreddits which are on the front page of the old reddit layout
--- background.js
+++ background.js
@@ -11,16 +11,16 @@ browser.menus.onClicked.addListener((info,tab)=>{
 		case "blockSubreddit":
 			//block a subreddit from a link
 			let subreddit = info.linkUrl.toLowerCase();
-			subreddit = subreddit.match(/\/r\/[^/\n]+/g);
+			subreddit = subreddit.match(/\/r\/[^/\n]+/g);	//Extract the subreddit name from the link
 			if(subreddit.length < 1) return;
 			subreddit = subreddit[0].substr(3);
 			//Get current list of blocked subreddits
 			browser.storage.sync.get("blockList").then((item)=>{
 				//Add new subreddit to the list
-				let newBlockList = [...item.blockList]
+				let newBlockList = [...item.blockList]	//Create copy of current blocklist
 				newBlockList.push(subreddit);
 				newBlockList.sort();
-				browser.storage.sync.set({blockList:newBlockList});
+				browser.storage.sync.set({blockList:newBlockList});	//Update the block list
 				//Reload the page
 				browser.tabs.reload(tab.id);
 			},(err)=>{});
